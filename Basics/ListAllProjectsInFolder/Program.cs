706f09ca4060ba3f2bf45ef3c63156ae7457e9da
Sreekanth <sreekanthb87@gmail.com>
2025-02-01 13:53:10 +0100

    V3: File Operation in a folder
    File operation

---
+++ Basics/ListAllProjectsInFolder/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ListAllProjectsInFolder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string folderPath = @"C:\Git-Sree\DataStructExplorer\DSPlayground\Searching"; // Change this to your folder path
+
+        if (Directory.Exists(folderPath))
+        {
+            string[] projectFiles = Directory.GetFiles(folderPath, "*.csproj", SearchOption.AllDirectories);
+
+            Console.WriteLine("C# Projects found:");
+            foreach (string file in projectFiles)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(file)); // Prints project name
+            }
+        }
+        else
+        {
+            Console.WriteLine("Folder does not exist.");
+        }
+    }
+}
