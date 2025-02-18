700c43544446a6d6c1852a698d704e32e7fe9ad2
Sreekanth <sreekanthb87@gmail.com>
2025-02-18 09:40:15 +0100

    V15: Bindings
    In C# and .NET Core, early binding and late binding refer to when the decision about the method or property being accessed is made, either at compile-time or runtime

---
+++ Basics/Binding/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Binding;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Early binding, resolved at compile-time
+        Dog dog = new Dog();
+        dog.Speak();
+
+        // Also early binding
+        Animal animal = new Animal();
+        animal.Speak();
+
+
+        // Late binding using reflection
+        Type type = typeof(Animal);
+
+        // Create an instance dynamically
+        object obj = Activator.CreateInstance(type);
+
+        // Get method information
+        MethodInfo method = type.GetMethod("Speak"); 
+
+        method.Invoke(obj, null); // Invoke method dynamically
+    }
+}
+
+class Animal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Animal speaks");
+    }
+}
+
+class Dog : Animal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
