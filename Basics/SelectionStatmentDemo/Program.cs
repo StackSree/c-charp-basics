fe4e47a932a85dcb11645314a010dc7ebd789f3a
Sreekanth <sreekanthb87@gmail.com>
2025-04-09 15:15:08 +0200

    V117: SelectionStatmentDemo
    Selection Statement Demo

---
+++ Basics/SelectionStatmentDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SelectionStatmentDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        // if-else
+        if (num > 0)
+        {
+            Console.WriteLine("Positive number");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("Negative number");
+        }
+        else
+        {
+            Console.WriteLine("Zero");
+        }
+
+        // switch
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("One");
+                break;
+            case 2:
+                Console.WriteLine("Two");
+                break;
+            default:
+                Console.WriteLine("Other number");
+                break;
+        }
+    }
+}
