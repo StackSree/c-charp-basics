d153416d8481ccd990eaeadc555bfc9053b41c0d
Sreekanth <sreekanthb87@gmail.com>
2025-04-13 16:27:47 +0200

    V125: Namespace demo
    Utilities Namespaces Demo

---
+++ Basics/NamespacesDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using UtilitiesNamespacesDemo; // Diffrent namespace
+namespace NamespacesDemo; // File-scoped namespace
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Using a class from the current namespace
+        var demoClass = new DemoClass();
+        demoClass.DoSomething();
+
+        // Using a class from the MyLibrary.Utilities namespace
+        var helper = new StringHelper();
+        Console.WriteLine(helper.ReverseString("hello"));
+    }
+
+    public class DemoClass
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something in NamespaceDemo.");
+        }
+    }
+}
---
+++ Basics/NamespacesDemo/Utilities.cs
@@ -0,0 +1,14 @@
+﻿
+namespace UtilitiesNamespacesDemo;
+
+public class StringHelper
+{
+
+    public string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+}
