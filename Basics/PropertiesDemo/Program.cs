e436c644a02b640e0332b736a0e8d91cbcf0250a
Sreekanth <sreekanthb87@gmail.com>
2025-05-29 22:25:42 +0200

    V205: PropertiesDemo
    PropertiesDemo

---
+++ Basics/PropertiesDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PropertiesDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Stock msft = new Stock();
+        msft.CurrentPrice = 30;     // Calls set
+        msft.CurrentPrice -= 3;     // Calls get then set
+
+        Console.WriteLine(msft.CurrentPrice); // Calls get
+    }
+}
+
+public class Stock
+{
+    private decimal currentPrice;
+
+    public decimal CurrentPrice
+    {
+        get { return currentPrice; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Price can't be negative.");
+            currentPrice = value;
+        }
+    }
+}
